d84592144184dde09dba907a1ed15a6ca7a9a112
JiangTaoHou <925147954@qq.com>
2024-02-29 10:29:03 +0800

    1.增加@PreAuthorize注解里面的值的使用办法

--- eladmin/eladmin-common/src/main/java/me/zhengjie/config/AuthorityConfig.java
+++ eladmin/eladmin-common/src/main/java/me/zhengjie/config/AuthorityConfig.java
@@ -18,7 +18,6 @@ package me.zhengjie.config;
 import me.zhengjie.utils.SecurityUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,7 +30,16 @@ public class AuthorityConfig {
     public Boolean check(String ...permissions){
         // 获取当前用户的所有权限
         List<String> elPermissions = SecurityUtils.getCurrentUser().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
-        // 判断当前用户的所有权限是否包含接口上定义的权限
-        return elPermissions.contains("admin") || Arrays.stream(permissions).anyMatch(elPermissions::contains);
+        // 判断当前用户的所有权限是否包含接口上定义的权限,如果是admin那么所有的都可以查看
+        if (elPermissions.contains("admin")) return true;
+        //这个就是@PreAuthorize里面的那个值，可以放数组，可以是单个的
+        //如果查询到当前用户的权限和@PreAuthorize里面的对应，那么就可以访问
+        //之前的开源是通过请求的path来判定的，这个相当于是通过自定义的一个字符串来匹配
+        for (String permission : permissions) {
+            if (elPermissions.contains(permission)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
